d43cc669b579a3e326673861050de939a0265b36
gkats <giannis.katsanos@gmail.com>
2016-10-14 10:51:15 +0300

    More installations for Debian

--- debian/install.sh
+++ debian/install.sh
@@ -12,3 +12,20 @@ sudo apt-get install -y git
 
 sudo apt-get install ttf-bitstream-vera
 
+# ruby-install
+wget -O ruby-install-0.6.0.tar.gz https://github.com/postmodern/ruby-install/archive/v0.6.0.tar.gz
+tar -xzvf ruby-install-0.6.0.tar.gz
+cd ruby-install-0.6.0/
+sudo make install
+rm ruby-install-0.6.0.tar.gz 
+
+# chruby
+wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
+tar -xzvf chruby-0.3.9.tar.gz
+cd chruby-0.3.9/
+sudo make install
+rm chruby-0.3.9.tar.gz
+
+# nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.30.2/install.sh | bash
+
